1e6b3db04034a6a2f57342fc11106e40ea3b6533
Oleksandr Polikovskiy <trotpolik@gmail.com>
2026-02-19 11:35:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,32 @@
-# write your code here
+import pytest
+from typing import Any, Type
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected_result",
+    [
+        ("", False),
+        ("Пароль@фadadf", False),
+        ("Pass@word1", True),
+        ("S234$67", False),
+        ("S234$6789asdfgh11", False),
+    ]
+)
+def test_check_password(password: str, expected_result: bool) -> None:
+    assert check_password(password) == expected_result
+
+
+@pytest.mark.parametrize(
+    "bad_password, expected_result_bad_password",
+    [
+        (123, TypeError),
+        (None, TypeError),
+        (object(), TypeError),
+    ]
+)
+def test_check_password_type(bad_password: Any,
+                             expected_result_bad_password: Type[Exception])\
+        -> None:
+    with pytest.raises(expected_result_bad_password):
+        check_password(bad_password)
